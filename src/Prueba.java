41bc7a36032e12ba8f6c8218e2535fb24efa3797
Eva Peralta <eva.peralta@gmail.com>
2018-11-19 09:01:11 +0100

    Commit 3.2
    Añadir j=2

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		
-		int i=3;
+		int i=3,j=2;
 		boolean exit=false;
 		
 		if(i>=1) {
@@ -11,6 +11,9 @@ public class Prueba {
 		}
 		else if(i<0){
 			System.out.println(i);
+		}
+		else if(j>=2) {
+			System.out.println(j);
 		}
 
 	}
